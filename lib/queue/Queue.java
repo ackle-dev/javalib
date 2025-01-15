1eb22dd5e6294431a2ecf87de41afd3dd01aad68
Akhil Pillai <ackledotdev@gmail.com>
2025-01-15 11:52:28 -0800

    fmt, add queue

--- lib/queue/Queue.java
+++ lib/queue/Queue.java
@@ -1,1 +1,71 @@
 
+import java.util.Arrays;
+
+public class Queue<T> {
+
+	private final T[] _items;
+	public final int maxSize;
+	private int _startIndex;
+	public int size;
+
+	@SuppressWarnings("unchecked")
+	public Queue(int maxSize) {
+		this.maxSize = maxSize;
+		_items = (T[]) new Object[maxSize];
+		_startIndex = 0;
+		size = 0;
+	}
+
+	public Queue(T[] items) {
+		_items = items;
+		maxSize = items.length;
+		_startIndex = 0;
+		size = items.length;
+	}
+
+	public int length() {
+		return size;
+	}
+
+	public boolean isFull() {
+		return size == maxSize;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public int pushEnd(T item) throws ArrayIndexOutOfBoundsException {
+		if (isFull())
+			throw new ArrayIndexOutOfBoundsException("Queue is full");
+		_items[(_startIndex + size) % maxSize] = item;
+		return ++size;
+	}
+
+	public T popStart() {
+		if (isEmpty())
+			return null;
+		size--;
+		return _items[_startIndex++ % maxSize];
+	}
+
+	// tests
+	public static void main(String[] args) {
+		Queue<Integer> q = new Queue<>(5);
+		for (int i = 1; i <= 5; i++) {
+			q.pushEnd(i);
+			System.out.printf("%d %d\n", q._startIndex, q.size);
+		}
+		try {
+			q.pushEnd(6);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+		}
+		for (int i = 0; i < 5; i++)
+			System.out.println(q.popStart());
+		System.out.println(q.isEmpty());
+		q.pushEnd(6);
+		System.out.printf("%d %d\n", q._startIndex, q.size);
+		System.out.println(Arrays.toString(q._items));
+	}
+}
